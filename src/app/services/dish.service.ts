81c19de0395d722d9f03c81d4e7c0f4997512d12
tatiana <tatiana.pekhtereva@gmail.com>
2019-04-01 20:03:22 +1300

    RsJs and use of observables;

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from "../shared/dish";
 import { DISHES } from "../shared/dishes";
-
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,46 +11,27 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]>{
-    return  new Promise(resolve => {
-      //simulate server delay of 2 seconds
-      setTimeout(() => resolve(DISHES),2000);
-    });
+  getDishes(): Observable<Dish[]>{
+    //return  of(DISHES).pipe(delay(2000)).toPromise();
+     return of(DISHES).pipe(delay(2000));
   }
 
-  getDish(id:string): Promise<Dish>{
-   let returnedDish: Dish;
-   for (let dish of DISHES){
-      if(dish.id === id){
-        returnedDish = dish;
-      }
+   getDish(id:string): Observable<Dish>{
+    let returnedDish: Dish;
+    for (let dish of DISHES){
+       if(dish.id === id){
+         returnedDish = dish;
+       }
+    }
+    return  of(returnedDish).pipe(delay(2000));
    }
-  // return Promise.resolve(returnedDish);
-   return new Promise(resolve => {
-     //simulate server delay of 2 seconds
-      setTimeout(() => resolve(returnedDish),2000);
-    });
+
+/*  getDish(id: number): Promise<Dish> {
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise();
   }
-
-/*
-getDish(id:string): Dish{
-  return DISHES.filter((dishX)=>{
-        (dishX.id === id)
-      })[0];
-}
-  */
-
-  // getDish(id:string): Dish{
-  //   return DISHES.filter( function(bob){
-  //     return bob.id === id;
-  //   })[0];
-  // }
-
-  getFeaturedDish(): Promise<Dish>{
+*/
+  getFeaturedDish(): Observable<Dish>{
       //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
-      return  new Promise(resolve => {
-        //simulate server delay of 2 seconds
-        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]),2000);
-      });
+          return  of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
   }
 }
