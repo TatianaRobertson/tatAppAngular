c6f555151fd3b337d4376e9b16a4f56b3149f600
tatiana <tatiana.pekhtereva@gmail.com>
2019-04-03 21:48:14 +1300

    saving data to the server using PUT verb

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -26,6 +26,8 @@ export class DishdetailComponent implements OnInit {
 
   commentForm: FormGroup;
   comment: Comment;
+  dishcopy:Dish;
+
   @ViewChild('fform') commentFormDirective;
 
   formErrors ={
@@ -96,6 +98,7 @@ export class DishdetailComponent implements OnInit {
     this.route.params
       .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
       .subscribe(dish => { this.dish = dish;
+                           this.dishcopy = dish;
                            this.setPrevNext(dish.id);
                          },
                  errmess => this.errMess = <any>errmess);
@@ -116,7 +119,15 @@ export class DishdetailComponent implements OnInit {
     let currentDate = new Date();
 
     this.comment.date = currentDate.toISOString();
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+
+    this.dishservice.putDish(this.dishcopy)
+        .subscribe(dish => {this.dish = dish;
+                            this.dishcopy = dish;
+                            },
+                errmess => {this.dish = null;
+                            this.dishcopy = null;
+                            this.errMess = <any>errmess;})
 
     this.commentForm.reset({
       author: '',
