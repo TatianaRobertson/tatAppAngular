b1d308cbaea679be472d3c9e3fcc988e8bd086c7
tatiana <tatiana.pekhtereva@gmail.com>
2019-04-06 20:30:28 +1300

    animation and changes to leader and promotion service to add http.get

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
+import { flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  host:{'[@flyInOut]': 'true',
+         'style': 'dysplay: block;'},
+  animations:[ flyInOut()]
 })
 export class ContactComponent implements OnInit {
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish'
 //import { Dishdetail } from '../dishdetail/dishdetail';
 import { DishService } from '../services/dish.service';
-
+import { flyInOut,expand } from '../animations/app.animation';
 
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host:{'[@flyInOut]': 'true',
+         'style': 'dysplay: block;'},
+  animations:[ flyInOut(), expand()]
 })
 export class MenuComponent implements OnInit {
 
