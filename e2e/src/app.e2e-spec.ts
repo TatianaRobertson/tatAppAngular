c3c36c698c918baa97dbd33585860629b1912a82
tatiana <tatiana.pekhtereva@gmail.com>
2019-04-08 21:18:21 +1200

    end to end testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,9 +9,34 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to tatApp!');
+    page.navigateTo('/');
+  //  expect(page.getTitleText()).toEqual('Welcome to tatApp!')
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to About Us page by clickign on link', () => {
+    page.navigateTo('/');
+    let navLink = page.getAllElements('a').get(1);
+    navLink.click();
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+   const newSubmitButton = page.getElement('button[type=submit]');
+   newSubmitButton.click();
+   browser.pause();
+
+
+  });
+
+
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
